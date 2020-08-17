62f3eb10158462c53a023fbca0c49c933a498eee
Anand Soni <sanand@narola.local>
2020-08-17 17:05:30 +0530

    try to resolve revisit issue

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 
 const express = require('express')
+const history = require('connect-history-api-fallback')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
+app.use(history())
 app.use(serveStatic(path.join(__dirname, 'dist')))
 const port = process.env.PORT || 8080
-app.use(history()).listen(port)
+app.listen(port)
