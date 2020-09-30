7477c04475c97989dada6d0cd9f318474d220e66
Arpit <ab@narola.email>
2020-09-30 18:31:45 +0530

    Upload convert PDF related changes

--- src/layouts/components/customizer/TheCustomizer.vue
+++ src/layouts/components/customizer/TheCustomizer.vue
@@ -12,12 +12,14 @@
   <div id="theme-customizer" class="uploadmodal-wrapper">
     <!-- Open Customizer Button -->
     <vs-button
-      @click.stop="active=!active"
+      @click.stop="active = !active"
       color="primary"
       type="filled"
       class="customizer-btn upload-btn"
     >
-      <img :src="require('../../../assets/images/sidebar_icon/menu-open.png')" />
+      <img
+        :src="require('../../../assets/images/sidebar_icon/menu-open.png')"
+      />
       Upload
     </vs-button>
 
@@ -30,23 +32,31 @@
       class="items-no-padding"
     >
       <vs-button
-        @click.stop="active=!active"
+        @click.stop="active = !active"
         color="primary"
         type="filled"
         class="customizer-btn upload-btn closeupload-btn"
       >
-        <img :src="require('../../../assets/images/sidebar_icon/menu-open.png')" />
+        <img
+          :src="require('../../../assets/images/sidebar_icon/menu-open.png')"
+        />
         Upload
       </vs-button>
       <div class="h-full">
-        <div class="customizer-header pt-4 pb-4 flex items-center justify-between px-6">
+        <div
+          class="customizer-header pt-4 pb-4 flex items-center justify-between px-6"
+        >
           <div>
             <h4 class="text-white">UPLOAD</h4>
             <small class="text-white">
               <i>Advance Upload</i>
             </small>
           </div>
-          <feather-icon icon="XIcon" @click.stop="active = false" class="cursor-pointer"></feather-icon>
+          <feather-icon
+            icon="XIcon"
+            @click.stop="active = false"
+            class="cursor-pointer"
+          ></feather-icon>
         </div>
         <vs-divider class="mb-5 mt-0" />
         <component
@@ -57,30 +67,48 @@
         >
           <div class="vx-row mb-3 ml-3 mr-3">
             <div class="vx-col w-full">
-              <vs-input class="w-full inputbox-style" label="Type" v-model="popup_type" />
+              <vs-input
+                class="w-full inputbox-style"
+                label="Type"
+                v-model="popup_type"
+              />
             </div>
           </div>
 
           <div class="vx-row mb-3 ml-3 mr-3">
             <div class="vx-col w-full">
-              <vs-input class="w-full inputbox-style" label="Category" v-model="popup_category" />
+              <vs-input
+                class="w-full inputbox-style"
+                label="Category"
+                v-model="popup_category"
+              />
             </div>
           </div>
 
           <div class="vx-row mb-3 ml-3 mr-3">
             <div class="vx-col w-full">
-              <vs-input class="w-full inputbox-style" label="Folder" v-model="popup_folder" />
+              <vs-input
+                class="w-full inputbox-style"
+                label="Folder"
+                v-model="popup_folder"
+              />
             </div>
           </div>
 
           <div class="vx-row mb-3 ml-3 mr-3">
             <div class="vx-col w-full">
-              <vs-input class="w-full inputbox-style" label="Template" v-model="popup_template" />
+              <vs-input
+                class="w-full inputbox-style"
+                label="Template"
+                v-model="popup_template"
+              />
             </div>
           </div>
 
           <div class="vx-row mb-3 ml-3 mr-3">
-            <div class="vx-col w-full d-flex justify-content-between align-items-center">
+            <div
+              class="vx-col w-full d-flex justify-content-between align-items-center"
+            >
               <vs-input
                 class="w-full inputbox-style input-btn-style"
                 label="Labels"
@@ -90,7 +118,8 @@
                 class="float-right btn-yellow mt-25px labeladd-btn ml-3 h-38 pd-0 w-100px"
                 :disabled="popup_label == ''"
                 @click="addLabels()"
-              >Add</vs-button>
+                >Add</vs-button
+              >
             </div>
           </div>
 
@@ -102,27 +131,34 @@
                   v-for="(label, index) in labels"
                   :key="index"
                   closable
-                >{{ label }}</vs-chip>
+                  >{{ label }}</vs-chip>
               </div>
             </div>
           </div>
 
           <div class="vx-row ml-3 mr-3">
             <div class="vx-col w-full text-right">
-              <p class="star-wrapper mt-1 d-flex justify-content-end align-items-center flex-wrap">
+              <p
+                class="star-wrapper mt-1 d-flex justify-content-end align-items-center flex-wrap"
+              >
                 <span class="mr-3">Stars</span>
                 <!-- <i class="fas fa-star pl-2"></i>
                 <i class="fas fa-star pl-2"></i>
                 <i class="fas fa-star pl-2"></i>
                 <i class="fas fa-star pl-2"></i>
                 <i class="fas fa-star pl-2"></i>-->
-                <star-rating :star-size="20" :read-only="true" :rounded-corners="true" :rating="5"></star-rating>
+                <star-rating
+                  :star-size="20"
+                  :read-only="true"
+                  :rounded-corners="true"
+                  :rating="5"
+                ></star-rating>
               </p>
             </div>
           </div>
 
           <div class="vx-row ml-3 mr-3">
-            <vs-upload action="http://localhost:3000/Documents" />
+            <vs-upload action="http://localhost:3000/Documents" v-model="filePath" />
           </div>
           <div class="vx-row ml-3 mr-3 mb-6">
             <div class="vx-col w-full">
@@ -138,8 +174,13 @@
 
           <div class="vx-row mt-3 mb-6 pb-3">
             <div class="vx-col w-full text-center">
-              <vs-button class="mb-2 btn-yellow mr-3 h-38 pd-0 w-100px" @click="onSave()">Save</vs-button>
+              <vs-button
+                class="mb-2 btn-yellow mr-3 h-38 pd-0 w-100px"
+                @click="onSave()"
+                >Save</vs-button
+              >
               <vs-button class="mb-2 btn-gray h-38 pd-0 w-100px fw-500">Canel</vs-button>
+              <input type="file" @input="onInput($event)">
             </div>
           </div>
         </component>
@@ -157,9 +198,12 @@ import StarRating from 'vue-star-rating'
 // var fs = require("fs")
 // var wordBuffer = fs.readFileSync('../../../assets/files/demo.docx')
 
+var docxConverter = require('docx-pdf');
+
 export default {
   data () {
     return {
+      filePath:'',
       tglSwitch: true,
       active: false,
       popup_type: '',
@@ -186,6 +230,9 @@ export default {
     scrollbarTag () { return this.$store.state.is_touch_device ? 'div' : 'VuePerfectScrollbar' },
   },
   methods: {
+    onInput(e){
+      console.log('EE =>',e);
+    },
     addLabels () {
       if (this.popup_label) {
         this.labels.push(this.popup_label)
@@ -195,15 +242,22 @@ export default {
     remove (item) {
       this.labels.splice(this.labels.indexOf(item), 1)
     },
-    // onSave(){
-    //   toPdf(wordBuffer).then(
-    //     (pdfBuffer) => {
-    //       fs.writeFileSync("./test.pdf", pdfBuffer)
-    //     }, (err) => {
-    //       console.log(err)
-    //     }
-    //   )
-    // }
+    onSave () {
+      console.log('File =>',this.filePath);
+      docxConverter(this.filePath, '../../../assets/files/output.pdf', function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+        console.log('result' + result);
+      });
+      //   toPdf(wordBuffer).then(
+      //     (pdfBuffer) => {
+      //       fs.writeFileSync("./test.pdf", pdfBuffer)
+      //     }, (err) => {
+      //       console.log(err)
+      //     }
+      //   )
+    }
   },
 }
 </script>
